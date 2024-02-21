c2a971d37a6be11efa1378f584f7b3c8f146ef0f
SJ.Lee <eagleduk@gmail.com>
2024-02-21 21:38:53 +0900

    03. nextJS > using file-conventions not-found.js

--- 03. nextJS/lib/meals.js
+++ 03. nextJS/lib/meals.js
@@ -4,7 +4,7 @@ import { resolve } from "styled-jsx/css";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  throw new Error("");
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
+  // throw new Error("");
   return db.prepare("SELECT * FROM MEALS").all();
 }
