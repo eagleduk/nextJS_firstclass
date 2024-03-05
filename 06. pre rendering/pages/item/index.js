df9f4b9bd816301dfb9274189fa4d4583224e6ad
SJ.Lee <eagleduk@gmail.com>
2024-03-05 22:47:27 +0900

    06. pre rendering > Client-side data fetching and pre rendering

--- 06. pre rendering/pages/item/index.js
+++ 06. pre rendering/pages/item/index.js
@@ -3,47 +3,57 @@ import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default function Items() {
+export default function Items(props) {
+  const [result, setResult] = useState(props.result);
   const { data, isLoading, error } = useSWR(
     "https://udemy-perfect-react-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json",
     fetcher
   );
 
-  //   const [items, setItems] = useState();
-  //   const [isLoading, setIsLoading] = useState(false);
-
-  //   useEffect(() => {
-  //     setIsLoading(true);
-  //     fetch(
-  //       "https://udemy-perfect-react-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json"
-  //     )
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         const d = Object.entries(data).map(([key, value]) => ({
-  //           ...value,
-  //           id: key,
-  //         }));
-  //         setItems(d);
-  //       })
-  //       .finally(() => setIsLoading(false));
-  //   }, []);
+  useEffect(() => {
+    if (!data) return;
+    const d = Object.entries(data).map(([key, value]) => ({
+      ...value,
+      id: key,
+    }));
+    setResult(d);
+  }, [data]);
 
   if (error) return <p>Data fetching Error.</p>;
 
-  if (!data) return <p>No Data.</p>;
-
   if (isLoading) return <p>Loading....</p>;
+
+  if (!result) return <p>No Data.</p>;
 
   return (
     <ul>
-      {Object.entries(data).map(([key, value]) => {
+      {result.map((r) => {
         return (
-          <li key={key}>
-            <h1>{value.title}</h1>
-            <p>{value.openingText}</p>
+          <li key={r.id}>
+            <h1>{r.title}</h1>
+            <p>{r.openingText}</p>
           </li>
         );
       })}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://udemy-perfect-react-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json"
+  );
+  const data = await response.json();
+
+  const d = Object.entries(data).map(([key, value]) => ({
+    ...value,
+    id: key,
+  }));
+
+  return {
+    props: {
+      result: d,
+    },
+    revalidate: 10,
+  };
 }
