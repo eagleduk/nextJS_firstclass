0d5ae67977f8589eaeffaf709fcec045bed91ce8
SJ.Lee <eagleduk@gmail.com>
2024-03-19 21:51:44 +0900

    08. optimization > add metadata

---
+++ 08. optimization/README.md
@@ -0,0 +1,6 @@
+## 최적화
+
+#### metadata
+
+- nextJS 의 `Head` Component 내부에 추가
+- 어느 위치에 삽입하던 NextJS 에서 `<head>` 태그 내부로 위치시켜 준다.
--- 08. optimization/pages/index.js
+++ 08. optimization/pages/index.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
+
 import EventList from "@/components/EventList";
 import { getFeaturedEvents } from "@/dummy-data";
 
 export default function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+      </Head>
       <EventList events={props.events} />
     </div>
   );
