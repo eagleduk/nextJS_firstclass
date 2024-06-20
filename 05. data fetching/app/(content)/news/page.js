b33d3c76ce78a1ff08e1579a6e50e3a0456e04ab
SJ.Lee <eagleduk@gmail.com>
2024-06-20 21:42:21 +0900

    05. data fetching > client fetch

--- 05. data fetching/app/(content)/news/page.js
+++ 05. data fetching/app/(content)/news/page.js
@@ -1,13 +1,41 @@
-import Link from "next/link";
+"use client";
 
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
+
 import NewsList from "@/components/news/NewsList";
 
 export default function Page() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Error.");
+      }
+      const data = await response.json();
+      setNews(data);
+
+      setIsLoading(false);
+    })();
+  }, []);
+
+  if (isLoading) {
+    return <p>News Loading....</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
